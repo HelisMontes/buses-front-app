63c8710408bb7f73dee5edb9873c54207bd8cf9c
Helis Montes <helismontes@gmail.com>
2022-08-02 23:39:23 -0500

    ticket admin

---
+++ pages/ticket/index.vue
@@ -0,0 +1,168 @@
+<template>
+  <h1>Ticket page</h1>
+  <br />
+  <br />
+  <TableCustom
+    :data="list.data || []"
+    :meta="list.meta || {}"
+    :columns="COLUMNS"
+    @update-data="getAll"
+    @update-per-page="updatePerPage"
+    @update-page="updatePage"
+  >
+    <template v-slot:callback="{ data, field, row }">
+      <template v-if="field === 'status'">
+        <span v-if="data">Activo</span>
+        <span v-else>Inactivo</span>
+      </template>
+      <template v-else-if="field === 'actions'">
+        <Button
+          text="Editar"
+          @click="edit(row)"
+        />
+        <Button
+          text="Eliminar"
+          @click="deleteItem(row)"
+        />
+      </template>
+      <template v-else-if="field === 'journey'">
+        Origen - Destino: {{ data.origen.country }} - {{ data.origen.city }} / {{ data.destination.country }} - {{ data.destination.city }}
+        <br/>
+        Bus: {{ data.bus.plate }} - {{ data.bus.brand }} - {{ data.bus.model }} - {{ data.bus.color }}
+        <br/>
+        Chofer: {{ data.user.identification }} - {{ data.user.name }} - {{ data.user.last_name }} - {{ data.user.email }} - {{ data.user.phone }}
+        <br/>
+      </template>
+      <template v-else-if="field === 'user'">
+        {{ data.identification }} - {{ data.name }} {{ data.last_name }}
+      </template>
+      <template v-else>
+        {{ data }}
+      </template>
+    </template>
+  </TableCustom>
+  <Form
+    name="ticket"
+
+    :structure="FORM_STRUCTURE"
+
+    @submit="submit"
+
+    ref="form"
+  />
+  <br/>
+</template>
+
+<script setup>
+import { storeToRefs } from 'pinia'
+
+import TableCustom from '~/components/TableCustom.vue'
+import Image from '~/components/Image.vue'
+import Form from '~/components/Form.vue'
+
+import { useTicketStore } from '@/stores/ticket'
+import { useJourneyStore } from '@/stores/journey'
+import { useUserStore } from '@/stores/user'
+
+const ticketStore = useTicketStore()
+const { list, createStatus } = storeToRefs(ticketStore)
+const { getAll, updatePerPage, updatePage, save } = ticketStore
+const { COLUMNS } = ticketStore.list
+
+const journeyStore = useJourneyStore()
+const { listAllToObject: journeyListAllToObject } = storeToRefs(journeyStore)
+const { getListAll: journeyGetListAll } = journeyStore
+
+const userStore = useUserStore()
+const { listAllToObject: userListAllToObject } = storeToRefs(userStore)
+const { getListAll: userGetListAll } = userStore
+
+await journeyGetListAll()
+await userGetListAll({
+  filter: {
+    filter_by: 'type_user',
+    filter_value: 'PASS',
+  }
+})
+
+const FORM_STRUCTURE = {
+  user_id: {
+    label: 'Usuario',
+    validations: [
+      { required: true, message: 'Este campo es requerido' },
+    ],
+    options: userListAllToObject.value,
+    value: '',
+    errors: [],
+    component: 'FormInputDatalist',
+  },
+  journey_id: {
+    label: 'Viaje',
+    validations: [
+      { required: true, message: 'Este campo es requerido' },
+    ],
+    options: journeyListAllToObject.value,
+    value: '',
+    errors: [],
+    component: 'FormInputDatalist',
+  },
+  number_seat: {
+    label: 'Número de asiento',
+    validations: [
+      { required: true, message: 'Este campo es requerido' },
+      { min: 1, message: 'Mínimo 1' },
+      { max: 10, message: 'Máximo 10' },
+    ],
+    value: '',
+    errors: [],
+    component: 'FormInputNumeric',
+  },
+  status: {
+    label: 'Estado',
+    validations: [
+      { required: true, message: 'Este campo es requerido' },
+    ],
+    value: undefined,
+    errors: [],
+    component: 'FormInputCheckbox',
+  },
+}
+
+const form = ref();
+const submit = async (values) => {
+  try {
+    await save(values)
+    form.value.reset(FORM_STRUCTURE)
+    await getAll()
+  } catch ({ message }) {
+    if (typeof message === 'object' ) {
+      form.value.setErrors(message)
+    }else{
+      message?.ticket && alert(message.ticket)
+    }
+  }
+}
+const edit = async (row) => {
+  form.value.setData(row)
+}
+const deleteItem = async (row) => {
+  try {
+    await ticketStore.delete({
+      id: row.id,
+    })
+    form.value.reset(FORM_STRUCTURE)
+    await getAll()
+  } catch ({ message }) {
+    if (typeof message === 'object' ) {
+      form.value.setErrors(message)
+    }else{
+      message?.ticket && alert(message.ticket)
+    }
+  }
+}
+
+onMounted(() => {
+  getAll()
+})
+
+</script>
---
+++ stores/ticket.js
@@ -0,0 +1,146 @@
+import { defineStore } from 'pinia'
+
+
+const model = 'ticket'
+
+export const useTicketStore = defineStore(
+    `${model}-store`,
+    () => {
+        const list = reactive({
+            isLoading: false,
+            data: [],
+            meta: {
+                page: 0,
+                per_page: 10,
+                total_items: 10,
+                prev_page: null,
+                next_page: null,
+                last_page: 1,
+            },
+            COLUMNS: [
+                {
+                    label: 'N°',
+                    field: 'id',
+                },
+                {
+                    label: 'Usuario',
+                    field: 'user',
+                    type: 'callback',
+                },
+                {
+                    label: 'Viaje',
+                    field: 'journey',
+                    type: 'callback',
+                },
+                {
+                    label: 'Número de asiento',
+                    field: 'number_seat',
+                },
+                {
+                    label: 'Estado',
+                    field: 'status',
+                    type: 'callback',
+                },
+                {
+                    label: 'Acciones',
+                    field: 'actions',
+                    type: 'callback',
+                },
+            ]
+        })
+
+        const createStatus = reactive({
+            isLoading: false,
+            data: {},
+            errors: {},
+        })
+
+        async function getAll() {
+            const params = new URLSearchParams({
+                page: list.meta.page,
+                per_page: list.meta.per_page,
+            })
+            list.isLoading = true
+            list.data = []
+            return $fetch(`/api/${model}/?${params}`, {
+                method: 'GET',
+            }).then(({ data, message }) => {
+                list.isLoading = false
+                const { list: tickets, meta } = data.tickets
+                list.data = tickets
+                list.meta = meta
+                return tickets
+            }).catch(({ data }) => {
+                list.isLoading = false
+                return Promise.reject(JSON.parse(data.message))
+            })
+        }
+        async function updatePerPage(per_page) {
+            list.meta.per_page = per_page
+            return getAll()
+        }
+        async function updatePage(page) {
+            list.meta.page = page
+            return getAll()
+        }
+
+        async function save(data) {
+            createStatus.isLoading = true
+            createStatus.data = {}
+            createStatus.errors = {}
+            if(data.id) {
+                return $fetch(`/api/${model}/update`, {
+                    method: 'POST',
+                    body: data,
+                }).then(({ data, message }) => {
+                    createStatus.isLoading = false
+                    createStatus.data = data.ticket
+                    return message || ''
+                }).catch(({ data }) => {
+                    createStatus.isLoading = false
+                    return Promise.reject(JSON.parse(data.message))
+                })
+            }
+            return $fetch(`/api/${model}/create`, {
+                method: 'POST',
+                body: data,
+            }).then(({ data, message }) => {
+                createStatus.isLoading = false
+                createStatus.data = data.ticket
+                createStatus.errors = {}
+                return message || ''
+            }).catch(({ data }) => {
+                createStatus.isLoading = false
+                return Promise.reject(JSON.parse(data.message))
+            })
+        }
+
+        async function deleteItem({ id }) {
+            createStatus.isLoading = true
+            createStatus.data = {}
+            createStatus.errors = {}
+            return $fetch(`/api/${model}/delete`, {
+                method: 'POST',
+                body: { id },
+            }).then(({ data, message }) => {
+                createStatus.isLoading = false
+                createStatus.data = data.journey
+                createStatus.errors = {}
+                return message || ''
+            }).catch(({ data }) => {
+                createStatus.isLoading = false
+                return Promise.reject(JSON.parse(data.message))
+            })
+        }
+
+        return {
+            list,
+            getAll,
+            updatePerPage,
+            updatePage,
+            save,
+            delete: deleteItem,
+            createStatus,
+        }
+    },
+)
